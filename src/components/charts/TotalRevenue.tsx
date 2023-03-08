df546a826e957bec3853f70488d2328d6340bce0
AhmedFrontEnd-JS-ReactJS-VueJS-NodeJS <ahmed.vibrant1@gmail.com>
2023-03-08 14:55:32 +0500

    homePage
    homePage

--- src/components/charts/TotalRevenue.tsx
+++ src/components/charts/TotalRevenue.tsx
@@ -8,7 +8,7 @@ import { TotalRevenueOptions,TotalRevenueSeries } from './chart.config';
 const TotalRevenue = () => {
   return (
     <Box 
-    p="4" 
+    p={4} 
     flex="1"
     id="chart"
     display="flex"
@@ -19,10 +19,26 @@ const TotalRevenue = () => {
         <Typography 
         fontSize="18px"
         fontWeight={600}
-        color
+        color="#11142d"
         >
-
+          Total Revenue
         </Typography>
+        <Stack my="20px" direction="row" gap={4} flexWrap="wrap" padding="30px">
+          <Typography fontSize={28} fontWeight={700} color="#11142d">$236,535</Typography>
+       
+        <Stack direction="row" alignItems="center" gap={1}>
+          <ArrowCircleUpRounded sx={{fontSize:25,color:"#475be8",}} />
+          <Stack>
+            <Typography fontSize={15} color="#475be8">0.8%</Typography>
+            <Typography fontSize={12} color="#808191">The Last Month</Typography>
+          </Stack>
+        </Stack>
+        </Stack>
+        <ReactApexChart series={TotalRevenueSeries}
+        type="bar"
+        height={310}
+        options={TotalRevenueOptions}
+        />
     </Box>
   )
 }
