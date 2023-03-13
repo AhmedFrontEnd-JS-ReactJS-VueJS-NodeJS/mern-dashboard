9db3a41f4e97db4865ff1fb381698d90db09ab31
AhmedFrontEnd-JS-ReactJS-VueJS-NodeJS <ahmed.vibrant1@gmail.com>
2023-03-13 19:20:58 +0500

    1:36:50
    1:36:50

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -13,11 +13,35 @@ const Form = ({type,register,handleSubmit,handleImageChange,formLoading,onFinish
       <Box mt={2.5} borderRadius="15px" bgcolor="#fcfcfc">
         <form style={{marginTop:'20px',width:'100%',display:'flex',flexDirection:"column",gap:"20px",padding:"20px"}} onSubmit={handleSubmit(onFinishHandler)}>
             <FormControl>
-              <FormHelperText>Enter Property Name</FormHelperText>
+              <FormHelperText sx={{fontWeight:500, margin:'10px',fontSize:16, color:'#11142d'}}>Enter Property Name</FormHelperText>
               <TextField 
-              
+              fullWidth
+              required
+              id='outlined-basic'
+              color='info'
+              variant="outlined"
+              {...register('title',{required:true})}
               />
             </FormControl>
+            <FormControl>
+              <FormHelperText sx={{fontWeight:500, margin:'10px',fontSize:16, color:'#11142d'}}>Enter Description</FormHelperText>
+              <TextareaAutosize
+              minRows={5}
+              required
+              placeholder='Write a desc'
+              color='info'
+              style={{width:'100%',background:'transparent',fontSize:'16px',borderColor:'rgba(0,0,0,0.23)',borderRadius:6,padding:10,color:'#919191', }}
+              {...register('description',{required:true})}
+              />
+            </FormControl>
+            <Stack
+            direction="row" gap={4}>
+                <FormControl sx={{flex:1}}>
+              <FormHelperText sx={{fontWeight:500,margin:'10px 0',fontSize:16,color:'#11142D'}}>
+
+              </FormHelperText>
+                </FormControl>
+            </Stack>
         </form>
 
       </Box>
